85c71939f2038c56403281c3862ca4e674140878
matthewbdwyer <matthewbdwyer@pop-os.localdomain>
2020-07-11 00:24:42 -0400

    added coverage support in cmake and an lcov script to produce coverage reports

---
+++ gencov.sh
@@ -0,0 +1,6 @@
+!/bin/bash
+lcov --capture --directory build/src -output-file /tmp/tipc.coverage.info
+lcov --remove /tmp/tipc.coverage.info '/usr/include/*' -o /tmp/tipc.coverage.info
+lcov --remove /tmp/tipc.coverage.info '*.h' -o /tmp/tipc.coverage.info
+lcov --remove /tmp/tipc.coverage.info '*.def' -o /tmp/tipc.coverage.info
+genhtml /tmp/tipc.coverage.info -output-directory coverage.out
