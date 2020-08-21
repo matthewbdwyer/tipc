42ed0886274d42deac773515019d2f1adbe05843
Matthew Dwyer <matthewdwyer@Matthews-MacBook-Pro.local>
2020-08-21 10:07:49 -0400

    updated unit tests for new semantics of doMatch

--- test/unit/semantic/types/concrete/TipConsTest.cpp
+++ test/unit/semantic/types/concrete/TipConsTest.cpp
@@ -1,42 +1,56 @@
 #include "catch.hpp"
 #include "TipCons.h"
+#include "TipRecord.h"
+#include "TipRef.h"
+#include "TipInt.h"
 #include "TipVar.h"
 #include "TipTypeVisitor.h"
 
-class MockCons1: public TipCons {
-public:
-    MockCons1() : TipCons(std::vector<std::shared_ptr<TipType>> {nullptr}) {};
-    bool operator==(const TipType &other) const override { return true; }
-    bool operator!=(const TipType &other) const override { return false; }
-    std::ostream &print(std::ostream &out) const override { return out; }
-    void accept(TipTypeVisitor *visitor) override {};
-};
-
-class MockCons2: public TipCons {
-public:
-    MockCons2() : TipCons(std::vector<std::shared_ptr<TipType>> {nullptr, nullptr}) {};
-    bool operator==(const TipType &other) const override { return true; }
-    bool operator!=(const TipType &other) const override { return false; }
-    std::ostream &print(std::ostream &out) const override { return out; }
-    void accept(TipTypeVisitor *visitor) override {};
-};
-
 TEST_CASE("TipCons: Test doMatch considers arity" "[TipCons]") {
-    MockCons1 mockCons1a;
-    MockCons1 mockCons1b;
+    auto tipInt = std::make_shared<TipInt>();
 
-    MockCons2 mockCons2a;
-    MockCons2 mockCons2b;
+    std::vector<std::shared_ptr<TipType>> params0 { };
+    std::vector<std::shared_ptr<TipType>> params1 { tipInt };
 
-    REQUIRE(mockCons1a.doMatch(&mockCons1b));
-    REQUIRE(mockCons2a.doMatch(&mockCons2b));
-    REQUIRE_FALSE(mockCons1a.doMatch(&mockCons2a));
+    // Function with no params
+    auto tipFunction1 = std::make_shared<TipFunction>(params0, tipInt);
+
+    // Function with one param
+    auto tipFunction2 = std::make_shared<TipFunction>(params1, tipInt);
+
+    REQUIRE_FALSE(tipFunction1->doMatch(tipFunction2.get()));
+    REQUIRE_FALSE(tipFunction2->doMatch(tipFunction1.get()));
+}
+
+TEST_CASE("TipCons: Test doMatch considers constructor type" "[TipCons]") {
+    std::vector<std::shared_ptr<TipType>> params { };
+
+    auto tipInt = std::make_shared<TipInt>();
+
+    // Ref is cons with 1 argument
+    auto tipRef = std::make_shared<TipRef>(tipInt);
+
+    // Function with no params is cons with one argument
+    auto tipFunction = std::make_shared<TipFunction>(params, tipInt);
+
+    std::vector<std::shared_ptr<TipType>> inits { tipInt };
+    std::vector<std::string> names {"foo"};
+
+    // Record with one field is cons with one argument
+    auto tipRecord = std::make_shared<TipRecord>(inits, names);
+
+    REQUIRE_FALSE(tipRef->doMatch(tipFunction.get()));
+    REQUIRE_FALSE(tipRef->doMatch(tipRecord.get()));
+    REQUIRE_FALSE(tipRecord->doMatch(tipRef.get()));
+    REQUIRE_FALSE(tipRecord->doMatch(tipFunction.get()));
+    REQUIRE_FALSE(tipFunction->doMatch(tipRef.get()));
+    REQUIRE_FALSE(tipFunction->doMatch(tipRecord.get()));
 }
 
 TEST_CASE("TipCons: Test doMatch only works on TipCons" "[TipCons]") {
-    MockCons1 mockCons1;
-    TipVar tipVar;
+    auto tipInt = std::make_shared<TipInt>();
+    auto tipRef = std::make_shared<TipRef>(tipInt);
 
-    REQUIRE_FALSE(mockCons1.doMatch(&tipVar));
+    REQUIRE_FALSE(tipRef->doMatch(tipInt.get()));
 }
 
