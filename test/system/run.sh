a1b67b01e617ba682af32fea1348ebf4e1c5c5db
Nick Phair <nickphair@gmail.com>
2020-08-27 13:08:38 -0400

    write temp files to unique location
    + cleanup on exit

--- test/system/run.sh
+++ test/system/run.sh
@@ -3,14 +3,12 @@
 declare -r ROOT_DIR=${TRAVIS_BUILD_DIR:-$(git rev-parse --show-toplevel)}
 declare -r TIPC=${ROOT_DIR}/build/src/tipc
 declare -r RTLIB=${ROOT_DIR}/rtlib
+declare -r SCRATCH_DIR=$(mktemp -d)
 
 if [ -z "${TIPCLANG}" ]; then
   echo error: TIPCLANG env var must be set
   exit 1
 fi
-
-numtests=0
-numfailures=0
 
 curdir="$(basename `pwd`)"
 if [ "${curdir}" != "system" ]; then
@@ -18,16 +16,24 @@ if [ "${curdir}" != "system" ]; then
   exit 1
 fi
 
+numtests=0
+numfailures=0
+
+initialize_test() {
+  rm -f ${SCRATCH_DIR}/*
+  ((numtests++))
+}
+
 # Self contained test cases
 for i in selftests/*.tip
 do
+  initialize_test
   base="$(basename $i .tip)"
-  ((numtests++))
 
   ${TIPC} $i
   ${TIPCLANG} $i.bc ${RTLIB}/tip_rtlib.bc -o $base
 
-  ./${base} >/dev/null 2>/dev/null
+  ./${base} &>/dev/null
   exit_code=${?}
   if [ ${exit_code} -ne 0 ]; then
     echo -n "Test failure for : " 
@@ -42,13 +48,13 @@ done
 
 for i in selftests/*.tip
 do
+  initialize_test
   base="$(basename $i .tip)"
-  ((numtests++))
 
   ${TIPC} -do $i
   ${TIPCLANG} $i.bc ${RTLIB}/tip_rtlib.bc -o $base
 
-  ./${base} >/dev/null 2>/dev/null
+  ./${base} &>/dev/null
   exit_code=${?}
   if [ ${exit_code} -ne 0 ]; then
     echo -n "Test failure for unoptimized : "
@@ -64,38 +70,37 @@ done
 # IO related test cases
 for i in iotests/*.expected
 do
+  initialize_test
   expected="$(basename $i .tip)"
   executable="$(echo $expected | cut -f1 -d-)"
   input="$(echo $expected | cut -f2 -d- | cut -f1 -d.)"
-  ((numtests++))
 
   ${TIPC} iotests/$executable.tip
   ${TIPCLANG} iotests/$executable.tip.bc ${RTLIB}/tip_rtlib.bc -o $executable
 
   ./${executable} $input >iotests/$executable.output
 
-  diff iotests/$executable.output $i >/tmp/$executable.diff
+  diff iotests/$executable.output $i > ${SCRATCH_DIR}/$executable.diff
 
-  if [[ -s /tmp/$expected.diff ]]
+  if [[ -s ${SCRATCH_DIR}/$expected.diff ]]
   then
     echo -n "Test differences for : " 
     echo $i
-    cat /tmp/$executable.diff
+    cat ${SCRATCH_DIR}/$executable.diff
     ((numfailures++))
   fi 
 
   rm iotests/$executable.tip.bc
   rm iotests/$executable.output
-  rm /tmp/$executable.diff
   rm $executable
 done
 
 # Tests to cover driver logic for error and argument handling
 for i in iotests/*error.tip
 do
-  ((numtests++))
+  initialize_test
 
-  ${TIPC} $i >/dev/null 2>/dev/null
+  ${TIPC} $i &>/dev/null
   exit_code=${?}
   if [ ${exit_code} -eq 0 ]; then
     echo -n "Test failure for : " 
@@ -107,10 +112,10 @@ do
 done
 
 # Tests to cover argument handling
-((numtests++))
-${TIPC} -pp -ps iotests/fib.tip >/tmp/fib.ppps
-diff iotests/fib.ppps /tmp/fib.ppps >/tmp/fib.diff
-if [[ -s /tmp/fib.diff ]]
+initialize_test
+${TIPC} -pp -ps iotests/fib.tip >${SCRATCH_DIR}/fib.ppps
+diff iotests/fib.ppps ${SCRATCH_DIR}/fib.ppps >${SCRATCH_DIR}/fib.diff
+if [[ -s ${SCRATCH_DIR}/fib.diff ]]
 then
   echo -n "Test differences for : " 
   echo $i
@@ -121,26 +126,24 @@ fi
 # Type checking at the system level
 for i in selftests/*.tip
 do
+  initialize_test
   base="$(basename $i .tip)"
-  ((numtests++))
 
-  ${TIPC} -pp -pt $i >/tmp/$base.pppt
-  diff $i.pppt /tmp/$base.pppt >/tmp/$base.diff
-  if [[ -s /tmp/$base.diff ]]
+  ${TIPC} -pp -pt $i >${SCRATCH_DIR}/$base.pppt
+  diff $i.pppt ${SCRATCH_DIR}/$base.pppt >${SCRATCH_DIR}/$base.diff
+  if [[ -s ${SCRATCH_DIR}/$base.diff ]]
   then
     echo -n "Test differences for : " 
     echo $i
-    cat /tmp/$base.diff
+    cat ${SCRATCH_DIR}/$base.diff
     ((numfailures++))
   fi 
-  rm /tmp/$base.pppt
-  rm /tmp/$base.diff
 done
 
 # Logging test 
 #   kick the tires on logging to make sure there are null pointer derefs
-${TIPC} -pt -log=/dev/null selftests/polyfactorial.tip >/dev/null 2>/dev/null
-((numtests++))
+initialize_test
+${TIPC} -pt -log=/dev/null selftests/polyfactorial.tip &>/dev/null 
 
 if [ ${numfailures} -eq "0" ]; then
   echo -n "all " 
@@ -151,3 +154,4 @@ else
   echo " tests failed"
 fi
 
+rm -r ${SCRATCH_DIR}
