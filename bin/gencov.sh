60de14e56b9f260dc34559a486c0dcbade69b62c
Nick Phair <nickphair@gmail.com>
2022-08-18 13:21:35 -0400

    more platform independent lcov
    + do not hardcode paths in lcov excludes

--- bin/gencov.sh
+++ bin/gencov.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 declare -r ROOT_DIR=${GITHUB_WORKSPACE:-$(git rev-parse --show-toplevel)}
+declare -r COV_OUTPUT=${ROOT_DIR}/coverage.info
+declare -r HTML_OUTPUT=${ROOT_DIR}/coverage.out
 
-pushd $ROOT_DIR
-lcov --capture --directory build -output-file coverage.info
-lcov --remove coverage.info '/usr/*' -o coverage.info
-lcov --remove coverage.info '/opt/*' -o coverage.info
-lcov --remove coverage.info '/Applications/*' -o coverage.info
-lcov --remove coverage.info '/Library/*' -o coverage.info
+lcov --capture --directory ${ROOT_DIR} --no-external -output-file ${COV_OUTPUT}
+lcov --remove coverage.info ${ROOT_DIR}'/build/*' -o ${COV_OUTPUT}
+lcov --remove coverage.info ${ROOT_DIR}'/externals/*' -o ${COV_OUTPUT}
 lcov --remove coverage.info '*.h' -o coverage.info
 lcov --remove coverage.info '*.hpp' -o coverage.info
-lcov --remove coverage.info '*/externals/*' -o coverage.info
-lcov --remove coverage.info '*/antlr4cpp_generated_src/*' -o coverage.info
-genhtml coverage.info -output-directory coverage.out
+genhtml ${COV_OUTPUT} -output-directory ${HTML_OUTPUT}
 
-echo Coverage report has been generated as coverage.info
-echo An HTML view of this report is available in coverage.out
+echo Coverage report has been generated as ${COV_OUTPUT}
+echo An HTML view of this report is available in ${HTML_OUTPUT}
