ecf071c808bf0ecfd0ce3d6a2af7f5124d63f84b
Nick Phair <nickphair@gmail.com>
2022-08-20 14:35:01 -0400

    remove codecov of tests

--- bin/gencov.sh
+++ bin/gencov.sh
@@ -6,6 +6,7 @@ declare -r HTML_OUTPUT=${ROOT_DIR}/coverage.out
 lcov --capture --directory ${ROOT_DIR} --no-external -output-file ${COV_OUTPUT}
 lcov --remove coverage.info ${ROOT_DIR}'/build/*' -o ${COV_OUTPUT}
 lcov --remove coverage.info ${ROOT_DIR}'/externals/*' -o ${COV_OUTPUT}
+lcov --remove coverage.info ${ROOT_DIR}'/test/*' -o ${COV_OUTPUT}
 lcov --remove coverage.info '*.h' -o coverage.info
 lcov --remove coverage.info '*.hpp' -o coverage.info
 genhtml ${COV_OUTPUT} -output-directory ${HTML_OUTPUT}
